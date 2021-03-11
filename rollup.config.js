dea25e74cbccc50d755efeb25d9f1bff590fccc2
王金强 <wangjinqiang@gongchang.com>
2021-03-11 17:54:03 +0800

    init

---
+++ rollup.config.js
@@ -0,0 +1,86 @@
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import strip from '@rollup/plugin-strip'
+import babel from '@rollup/plugin-babel'
+import typescript from '@rollup/plugin-typescript'
+
+import filesize from 'rollup-plugin-filesize'
+import pkg from './package.json'
+const input = 'src/index.ts'
+
+const extensions = ['.js', '.ts']
+
+const packageName = pkg.name
+  .replace('@', '')
+  .replace('/', '-')
+  .split('-')
+  .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+  .join('')
+export default [
+  {
+    input,
+    output: {
+      file: pkg.browser,
+      format: 'umd',
+      name: packageName,
+      globals: {
+        axios: 'axios',
+      },
+      sourcemap: true,
+    },
+    plugins: [
+      typescript({
+        tsconfig: './tsconfig.json',
+      }),
+
+      resolve({
+        extensions,
+      }),
+
+      commonjs(),
+
+      babel({
+        babelHelpers: 'runtime',
+        exclude: [/\/node_modules\//],
+        extensions,
+      }),
+
+      strip(),
+      // terser(),
+      filesize(),
+    ],
+    external: (id) => {
+      return /axios/.test(id)
+    },
+  },
+  // {
+  //   input,
+  //   output: [
+  //     {
+  //       file: pkg.module,
+  //       format: 'esm',
+  //       sourcemap: true,
+  //     },
+  //     {
+  //       file: pkg.main,
+  //       format: 'cjs',
+  //       sourcemap: true,
+  //       exports: 'auto',
+  //     },
+  //   ],
+  //   plugins: [
+  //     typescript({ tsconfig: './tsconfig.json' }),
+  //     resolve({ extensions }),
+  //     commonjs(),
+  //     babel({
+  //       babelHelpers: 'runtime',
+  //       exclude: 'node_modules/**',
+  //       extensions,
+  //     }),
+  //     filesize(),
+  //   ],
+  //   external: (id) => {
+  //     return /axios|@hook\/|core-js|@babel\/runtime/.test(id)
+  //   },
+  // },
+]
---
+++ src/index.ts
@@ -0,0 +1,151 @@
+import axios, { CancelTokenSource, AxiosResponse, AxiosError } from 'axios'
+import Hook from '@hook/hook'
+import type { setterKey, options as hookOptions } from '@hook/hook'
+
+export interface options extends hookOptions {
+  abortable?: boolean
+}
+
+export type method = 'get' | 'post' | 'head' | 'put' | 'delete' | 'connect' | 'options' | 'trace' | 'GET' | 'POST' | 'HEAD' | 'PUT' | 'DELETE' | 'CONNECT' | 'OPTIONS' | 'TRACE'
+
+export default class Ajax extends Hook {
+  static axios = axios
+  static create (url: string, method: method, options?: options) : Ajax {
+    const ajax = new this(options)
+    return ajax.url(url).method(method)
+  };
+
+  private _ajaxable: boolean = true;
+  private _abortable: boolean | CancelTokenSource = false
+
+  constructor(options?: options) {
+    super(options)
+    const config = Object.assign(
+      {
+        abortable: true
+      },
+      options,
+    )
+    if (config.abortable) {
+      this.abortable()
+    }
+  }
+
+  config(key: setterKey, value?: any) {
+    return this.generateSetter('config')(key, value)
+  }
+
+  getConfig(field?: string) {
+    return this.generateGetter('config')(field)
+  }
+
+  params(key: setterKey, value?: any) {
+    return this.generateSetter('params')(key, value)
+  }
+
+  getParams(field?: string) {
+    return this.generateGetter('params')(field)
+  }
+
+  method(method: method) {
+    this.config('method', method)
+    return this
+  }
+
+  url(url: string) {
+    this.config('url', url)
+    return this
+  }
+
+  abortable() {
+    const { CancelToken } = axios
+    const source = CancelToken.source()
+    this._abortable = source
+    this.config('cancelToken', source.token)
+    return this
+  }
+
+  abort(message: string) {
+    const source = this._abortable
+    if (source) {
+      (<CancelTokenSource>source).cancel(message)
+    }
+    return this
+  }
+
+  isAbortable() {
+    return !!this._abortable
+  }
+
+  disable() {
+    this._ajaxable = false
+  }
+
+  enable() {
+    this._ajaxable = true
+  }
+
+  isDisabled() {
+    return this._ajaxable === false
+  }
+
+  async convertResponse(res: AxiosResponse): Promise<any> {
+    return res.data
+  }
+
+  async convertError(error: AxiosError): Promise<any> {
+    return error
+  }
+
+  async fetch() {
+    if (!this._ajaxable) {
+      return this
+    }
+    await this.emit('netRequest')
+
+    const config = this.getConfig()
+    const method = (config.method || 'get').toUpperCase()
+    if (method === 'PUT' || method === 'POST' || method === 'PATCH') {
+      this.config({
+        data: this.getParams()
+      })
+    } else {
+      this.config({
+        params: this.getParams()
+      })
+    }
+
+    await axios
+      .request(this.getConfig())
+      .then(async (res: AxiosResponse) => {
+        const convertedData: any = await this.convertResponse(res)
+        await this.emit('netResponse', true, res)
+        await this.emit('netSuccess', convertedData, res)
+        await this.emit('netCompleted', true, res)
+      })
+      .catch(async (error: AxiosError) => {
+        const convertedError: any = await this.convertError(error)
+        await this.emit('netResponse', false, error)
+        await this.emit('netError', convertedError, error)
+        await this.emit('netCompleted', false, error)
+      })
+    return this
+  }
+}
+
+async function init() {
+  const chain = Ajax.create('/account/login', 'POST')
+
+  await chain.config({
+    baseURL: 'https://api.vscj.com:3000'
+  }).params({
+    account: 'admin',
+    password: '123456'
+  }).on('netRequest', function() {
+    console.log('request')
+  }).on('netSuccess', (data) => {
+    console.log(data)
+  }).fetch()
+}
+
+init()
